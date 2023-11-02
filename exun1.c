b2ed27f457b293148ad0cf2aed92b92afdd4d271
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-11-02 10:48:45 -0300

    Update exun1.c

--- exun1.c
+++ exun1.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-// prototipo das funcoes
- maior(int n1, int n2);
 
-main(){
-    int numero1 = 10, numero2 = 20, resultado;
-    resultado = maior(numero1, numero2);
-    printf("%d", resultado);
+int maximo(int a, int b){
+    if(a > b){
+        return a;
+    }
+    else{
+        return b;
+    }
 }
 
-int maior(int n1, int n2){
-   if(n1 > n2) {
-    return n1;
-   } else {
-    return n2;
-   }
+int main(){
+    int numA, numB;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &numA);
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &numB);
+
+    int maior = maximo(numA, numB);
+
+    printf("Maior numero: %d", maior);
 
 }
